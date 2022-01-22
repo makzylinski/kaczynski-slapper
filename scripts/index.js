77ec5ff4548c89acafeb4d97b4479f7418703d4a
Maks Zylinski <maksymilian.zylinski@gamivo.com>
2022-01-22 12:21:16 +0100

    localstorage manipulation

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,8 @@
+import AppState from "./state.js";
 import Slapper from "./slapper/slapper.js";
 
+const state = new AppState();
 const c = new Slapper();
+
+state.saveSlapsData();
+state.getSlapsData();
